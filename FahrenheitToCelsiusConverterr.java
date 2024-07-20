f2bd0d0ef55cbebafdd5c962c1e56a3415d573a0
PadalaAshwini <padalaashwini3@gmail.com>
2024-07-20 18:43:39 +0530

    Add files via upload

---
+++ FahrenheitToCelsiusConverterr.java
@@ -0,0 +1,18 @@
+package Ashu;
+
+public class FahrenheitToCelsiusConverterr {
+
+	public static void main(String[] args) {
+        // Predefined value for temperature in Fahrenheit
+        double fahrenheit = 98.6;
+
+        // Convert Fahrenheit to Celsius
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        // Output the result
+        System.out.println("Temperature in Celsius: " + celsius + " °C");
+    }
+}
+	
+
+
---
+++ Myself.java
@@ -0,0 +1,26 @@
+package FirstProgram;
+
+public class Myself {
+
+
+		
+	
+		   public static void main(String[] args)
+			{
+				System.out.println("my name is Ashwini"); 
+				System.out.println("I am from kolkulpally");
+				System.out.println("I am pursuing mca in avanthi pg college");
+				System.out.println("I hava completed my graduation from Bsc");
+				System.out.println("I have completed my inter from MPC");
+				System.out.println("I have completed my ssc ");
+				System.out.println("my father name is Yadaiah");
+				System.out.println("my mother name is Sugunamma");
+				System.out.println("my brother name is Ravi");
+				System.out.println("my sister name is Mahalaxmi");
+			}
+
+		}	
+
+	
+
+
